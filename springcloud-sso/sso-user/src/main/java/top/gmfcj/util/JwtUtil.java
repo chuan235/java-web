45d7aa7c2f0816312744be5ba46a47dd554aa9a9
only <1595030095@qq.com>
2020-03-19 11:16:51 +0800

    update jwtUtil

--- springcloud-sso/sso-user/src/main/java/top/gmfcj/util/JwtUtil.java
+++ springcloud-sso/sso-user/src/main/java/top/gmfcj/util/JwtUtil.java
@@ -1,5 +1,6 @@
 package top.gmfcj.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
@@ -17,8 +18,8 @@ import java.util.UUID;
 @Component
 public class JwtUtil {
 
-    //解密的密钥 加密的时候传入密钥 解密的时候使用密钥来解析
-    public static final String JWT_KEY = "luban-key";
+    // 解密的密钥 加密的时候传入密钥 解密的时候使用密钥来解析
+    public static final String JWT_KEY = "login-key";
 
     /**
      * 由字符串生成加密key
@@ -27,9 +28,8 @@ public class JwtUtil {
      */
     public static SecretKeySpec generalKey() {
         String stringKey = JWT_KEY;
-        // 本地的密码解码[B@152f6e2
         byte[] encodedKey = Base64.decodeBase64(stringKey);
-        // 根据给定的字节数组使用AES加密算法构造一个密钥，使用 encodedKey中的始于且包含 0 到前 leng 个字节这是当然是所有。
+        // 根据给定的字节数组使用AES加密算法构造一个密钥
         SecretKeySpec key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
         return key;
     }
@@ -37,7 +37,8 @@ public class JwtUtil {
     public static Map<String, Object> generalKeyMap() {
         String stringKey = JWT_KEY;
         byte[] encodedKey = Base64.decodeBase64(stringKey);
-        SecretKeySpec key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");Map<String, Object> map = new HashMap();
+        SecretKeySpec key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+        Map<String, Object> map = new HashMap();
         map.put("arg1", encodedKey);
         map.put("arg2", encodedKey.length);
         map.put("arg3", "AES");
@@ -58,37 +59,47 @@ public class JwtUtil {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
         long nowMillis = System.currentTimeMillis();//生成JWT的时间
         Date now = new Date(nowMillis);
-        SecretKey key = generalKey();//生成签名的时候使用的秘钥secret,这个方法本地封装了的，一般可以从本地配置文件中读取，切记这个秘钥不能外露哦。它就是你服务端的私钥，在任何场景都不应该流露出去。一旦客户端得知这个secret, 那就意味着客户端是可以自我签发jwt了。
-        JwtBuilder builder = Jwts.builder() //这里其实就是new一个JwtBuilder，设置jwt的body
-//                .setClaims(claims)            //如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
-                .setId(id)                    //设置jti(JWT ID)：是JWT的唯一标识，根据业务需要，这个可以设置为一个不重复的值，主要用来作为一次性token,从而回避重放攻击。
-                .setIssuedAt(now)            //iat: jwt的签发时间
-                .setSubject(subject)        //sub(Subject)：代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，可以存放什么userid，roldid之类的，作为什么用户的唯一标志。
-                .signWith(signatureAlgorithm, key);//设置签名使用的签名算法和签名使用的秘钥
+        // 生成签名的时候使用的秘钥secret,这个方法本地封装了的，一般可以从本地配置文件中读取，这个秘钥不能外露。
+        // 它就是你服务端的私钥，在任何场景都不应该流露出去。一旦客户端得知这个secret, 那就意味着客户端是可以自我签发jwt了。
+        SecretKey key = generalKey();
+        // 这里其实就是new一个JwtBuilder，设置jwt的body
+        JwtBuilder builder = Jwts.builder()
+                // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
+//                .setClaims(claims)
+                // 设置jti(JWT ID)：是JWT的唯一标识，根据业务需要，这个可以设置为一个不重复的值，主要用来作为一次性token,从而回避重放攻击
+                .setId(id)
+                // iat: jwt的签发时间
+                .setIssuedAt(now)
+                // sub(Subject)：代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，可以存放什么userid，roldid之类的，作为什么用户的唯一标志
+                .setSubject(subject)
+                // 设置签名使用的签名算法和签名使用的秘钥
+                .signWith(signatureAlgorithm, key);
         if (ttlMillis >= 0) {
             long expMillis = nowMillis + ttlMillis;
             Date exp = new Date(expMillis);
-            builder.setExpiration(exp);        //设置过期时间
+            // 设置过期时间
+            builder.setExpiration(exp);
         }
-        return builder.compact();            //就开始压缩为xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxx这样的jwt
+        //就开始压缩为xxx.xxx.xxx这样的jwt
+        return builder.compact();
     }
 
 
     public static void main(String[] args) throws Exception {
-        Map<String,Object> user = new HashMap<>();
-        user.put("username","123");
-        user.put("password","321");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", "123");
+        user.put("password", "321");
         //把数据转换为JWT token
-//        String jwt = createJWT(UUID.randomUUID().toString(), JSON.toJSONString(user), 3600 * 24);
+        String jwt = createJWT(UUID.randomUUID().toString(), objectMapper.writeValueAsString(user), 3600 * 24);
 
-//        System.out.println("加密后的"+jwt);
-//        //解密
-//        Claims claims = parseJWT(jwt);
+        System.out.println("加密后的" + jwt);
+        //解密
+        Claims claims = parseJWT(jwt);
 
-//        System.out.println("解密后的"+claims.getSubject());
+        System.out.println("解密后的" + claims.getSubject());
 
     }
-
 
 
     /**
@@ -99,10 +110,15 @@ public class JwtUtil {
      * @throws Exception
      */
     public static Claims parseJWT(String jwt) throws Exception {
-        SecretKey key = generalKey();  //签名秘钥，和生成的签名的秘钥一模一样
-        Claims claims = Jwts.parser()  //得到DefaultJwtParser
-                .setSigningKey(key)         //设置签名的秘钥
-                .parseClaimsJws(jwt).getBody();//设置需要解析的jwt
+        // 签名秘钥，和生成的签名的秘钥一模一样
+        SecretKey key = generalKey();
+        // 得到DefaultJwtParser
+        Claims claims = Jwts.parser()
+                // 设置签名的秘钥
+                .setSigningKey(key)
+                // 设置需要解析的jwt
+                .parseClaimsJws(jwt)
+                .getBody();
         return claims;
     }
 
